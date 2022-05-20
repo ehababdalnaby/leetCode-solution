d38bcf49d3a2039e3f75da11043c4496c6db9375
Ehab Omara <89157664+ehababdalnaby@users.noreply.github.com>
2022-05-20 22:08:56 +0200

    Time: 208 ms (33.99%), Space: 41.7 MB (38.44%) - LeetHub

--- 234-palindrome-linked-list/234-palindrome-linked-list.c
+++ 234-palindrome-linked-list/234-palindrome-linked-list.c
@@ -6,38 +6,15 @@
  * };
  */
 
-struct ListNode * reverse(struct ListNode * head){
-    struct ListNode * next = NULL, * previous = NULL;
-    
-    while (head != NULL){
-        next = head -> next;
-        head -> next = previous;
-        
-        previous = head;
-        head = next;
-    }
-    return previous;
-}
-
 bool isPalindrome(struct ListNode* head){
-    struct ListNode * slow = head, *fast = head;
-    while (fast -> next != NULL && fast -> next -> next != NULL){
-        slow = slow -> next;
-        fast = fast -> next -> next;
+    int a[100000], count = 0;
+    while(head)
+    {
+        a[count++] = head->val;
+        head = head->next;
     }
-    struct ListNode * newhead = reverse(slow -> next);
-    slow -> next = NULL;
-    
-    struct ListNode *dummy = head, *dummyNewHead = newhead;
-    
-    while (dummy != NULL && dummyNewHead != NULL){
-        if (dummy -> val != dummyNewHead -> val){
+    for(int i=0; i<count/2; i++)
+        if(a[i] != a[count-i-1])
             return false;
-        }
-        dummy = dummy -> next;
-        dummyNewHead = dummyNewHead -> next;
-    }
-    
     return true;
-    
 }
