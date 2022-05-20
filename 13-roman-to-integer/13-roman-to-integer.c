6f229342c7af4c2e3914c4e4bb884d9fe4165a7e
Ehab Omara <89157664+ehababdalnaby@users.noreply.github.com>
2022-05-20 20:52:01 +0200

    Time: 3 ms (95.79%), Space: 6 MB (30.79%) - LeetHub

--- 13-roman-to-integer/13-roman-to-integer.c
+++ 13-roman-to-integer/13-roman-to-integer.c
@@ -1,21 +1,34 @@
+#define SIZE 7
 
+int hash_algorithm(char ch)
+{
+    switch(ch)
+    {
+        case 'I': return 0; break;
+        case 'V': return 1; break;
+        case 'X': return 2; break;
+        case 'L': return 3; break;
+        case 'C': return 4; break;
+        case 'D': return 5; break;
+        case 'M': return 6; break;
+        default: return 0;
+    }
+}
 
 int romanToInt(char * s){
-int t['X' + 1] = {
-        ['I'] = 1,
-        ['V'] = 5,
-        ['X'] = 10,
-        ['L'] = 50,
-        ['C'] = 100,
-        ['D'] = 500,
-        ['M'] = 1000,
-    };
-    int res = 0;
-    for (int i = 0; s[i]; i++) {
-        if (t[s[i]] < t[s[i+1]])
-            res -= t[s[i]];
-        else
-            res += t[s[i]];
+    char symbols[SIZE] = {'I','V','X','L','C','D','M'};
+    int values[SIZE] = {1,5,10,50,100,500,1000};
+    int total = 0;
+    for(int i = 0; s[i] != '\0'; i++)
+    {
+        if(values[hash_algorithm(s[i])] < values[hash_algorithm(s[i+1])])
+        {
+            total = total + values[hash_algorithm(s[i+1])] - values[hash_algorithm(s[i])];
+            i++;
+        }else
+        {
+            total = total + values[hash_algorithm(s[i])];
+        }
     }
-    return res;
+    return total;
 }
