1b1035032722dfa90d6ac6b18800a50346fea9c1
Ehab Omara <89157664+ehababdalnaby@users.noreply.github.com>
2022-05-20 20:13:33 +0200

    Time: 8 ms (27.72%), Space: 6 MB (79.89%) - LeetHub

--- 35-search-insert-position/35-search-insert-position.c
+++ 35-search-insert-position/35-search-insert-position.c
@@ -1,21 +1,20 @@
 
 
 int searchInsert(int* nums, int numsSize, int target){
-    int low = 0, high = numsSize-1;
+    int start = 0, end = numsSize - 1, middle = 0;
+    while(start <= end)
+    {
+        middle = start +(end - start)/2;
+        if(target == nums[middle])
+            return middle;
+        if(target > nums[middle])
+            start = middle +1;
+        else 
+            end = middle - 1;
+    }
+    
 
-        // Invariant: the desired index is between [low, high+1]
-        while (low <= high) {
-            int mid = low + (high-low)/2;
-
-            if (nums[mid] < target)
-                low = mid+1;
-            else
-                high = mid-1;
-        }
-
-        // (1) At this point, low > high. That is, low >= high+1
-        // (2) From the invariant, we know that the index is between [low, high+1], so low <= high+1. Follwing from (1), now we know low == high+1.
-        // (3) Following from (2), the index is between [low, high+1] = [low, low], which means that low is the desired index
-        //     Therefore, we return low as the answer. You can also return high+1 as the result, since low == high+1
-        return low;
+    return start;
+    
+    
 }
