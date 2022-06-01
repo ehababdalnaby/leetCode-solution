18cec4236f22fe2e2b89252c6ebeaa04e38c972f
Ehab Omara <89157664+ehababdalnaby@users.noreply.github.com>
2022-06-01 04:17:05 +0200

    Time: 95 ms (14.54%), Space: 12.3 MB (91.96%) - LeetHub

--- 344-reverse-string/344-reverse-string.c
+++ 344-reverse-string/344-reverse-string.c
@@ -2,11 +2,10 @@
 
 void reverseString(char* s, int sSize){
     int end = sSize -1;
-    char temp;
     for(int start = 0; start < sSize/2; start++)
     {
-        temp = s[start];
-        s[start] = s[end - start];
-        s[end - start] = temp;
+        s[start] = s[start] + s[end - start];
+        s[end - start] = s[start] - s[end - start];
+        s[start] = s[start] - s[end - start];
     }
 }
