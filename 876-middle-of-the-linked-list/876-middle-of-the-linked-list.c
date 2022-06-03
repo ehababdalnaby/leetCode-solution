b7b1082390c8d9c4807e8e3e22ff47fc33d91fbd
Ehab Omara <89157664+ehababdalnaby@users.noreply.github.com>
2022-06-03 18:16:30 +0200

    Time: 0 ms (100.00%), Space: 5.9 MB (38.43%) - LeetHub

---
+++ 876-middle-of-the-linked-list/876-middle-of-the-linked-list.c
@@ -0,0 +1,27 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+
+
+
+/*
+i will use fast and slow pointers algorithm
+fast poiter will move by two steps at time
+slow pointer will move by one step at time
+*/
+
+struct ListNode* middleNode(struct ListNode* head){
+    struct ListNode* fastPointer = head,*slowPointer = head;
+    while(fastPointer->next != NULL && slowPointer != NULL)
+    {
+        fastPointer = fastPointer->next->next;
+        slowPointer = slowPointer->next;
+        if(fastPointer == NULL)
+            break;
+    }
+    return slowPointer;
+}
