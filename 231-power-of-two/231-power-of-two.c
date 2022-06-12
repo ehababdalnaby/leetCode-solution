da4ff35975118968b060293302121c34acb96ffd
Ehab Omara <89157664+ehababdalnaby@users.noreply.github.com>
2022-06-12 22:33:09 +0200

    Time: 0 ms (100.00%), Space: 5.4 MB (74.50%) - LeetHub

--- 231-power-of-two/231-power-of-two.c
+++ 231-power-of-two/231-power-of-two.c
@@ -1,23 +1,5 @@
 
 
 bool isPowerOfTwo(int n){
-    if(n<=0)
-    {
-        return false;
-    }
-    bool result = true;
-    char count = 0;
-    while(n)
-    {
-        if(n & 0x01)
-        {
-            count++;
-        }
-        if(count > 1)
-        {
-            return false;
-        }
-        n = n >> 1; 
-    }
-    return result;
+   return n > 0 && !(n&(n-1));
 }
